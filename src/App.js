beb02e711f1edbaeb8b798c3a74f5448966db7aa
Partha Pratim Sarma <64675650+partha8@users.noreply.github.com>
2022-03-16 22:55:13 +0530

    feat: Home page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Submenu } from "./components/index";
+import { Home } from "./pages";
+
+// import { ForgotPassword, Login, SignUp } from "./pages/AuthPages/";
 
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Submenu />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          {/* <Route path="/product-listing" element={<ProductListing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/wishlist" element={<Wishlist />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import { App } from "./App";
 import { makeServer } from "./server";
+import { StateProvider } from "./context/StateProvider";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StateProvider>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
