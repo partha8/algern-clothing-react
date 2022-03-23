fdc0da051724f10947ed58469c0a91f7e7ce4bbd
Partha Pratim Sarma <64675650+partha8@users.noreply.github.com>
2022-03-23 19:53:56 +0530

    custom hook to fetch cart data

---
+++ src/hooks/useGetCart.js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useAuthContext } from "../context/AuthProvider";
+import axios from "axios";
+import { useStateContext } from "../context/StateProvider";
+
+const encodedToken = localStorage.getItem("token");
+
+export const useGetCart = () => {
+  const { userState } = useAuthContext();
+  const { productsDispatch } = useStateContext();
+  useEffect(() => {
+    if (userState._id) {
+      (async () => {
+        try {
+          const response = await axios.get("/api/user/cart", {
+            headers: {
+              authorization: encodedToken,
+            },
+          });
+          if (response.status === 200) {
+            productsDispatch({
+              type: "SET_CART",
+              payload: response.data.cart,
+            });
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    } else {
+      productsDispatch({ type: "SET_CART", payload: [] });
+    }
+  }, [userState._id]);
+};
